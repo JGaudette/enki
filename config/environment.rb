aec4decf2fe4e6180b69f289af08bdd3f1f08d6c
Jon <jon@digital-drip.com>
2010-09-17 09:14:39 -0400

    Add rspec dependency

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   config.gem "lesstile", :version => "~> 0.3"
   config.gem "will_paginate", :version => "~> 2.3.14", :source => 'http://gemcutter.org'
   config.gem "newrelic_rpm"
+  config.gem "rspec-rails"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
